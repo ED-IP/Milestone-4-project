c888122db1184f8a5b29bbfe4b1deebf678e4638
ED-IP <edip_courses@outlook.es>
2021-08-12 14:52:57 +0000

    modified product model

--- products/models.py
+++ products/models.py
@@ -3,31 +3,35 @@ from django.db import models
 # Create your models here.
 
 
-class Category(models.Model):
+# class Category(models.Model):
 
-    class Meta:
-        verbose_name_plural = 'Categories'
+#     class Meta:
+#         verbose_name_plural = 'Categories'
 
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+#     name = models.CharField(max_length=254)
+#     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
+#     def get_friendly_name(self):
+#         return self.friendly_name
 
 
-class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+class Product(models.Model):    
+    #category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    # sku = models.CharField(max_length=254, null=True, blank=True)
+    title = models.CharField(max_length=254)
+    author = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    EDITIONS = [            # https://docs.djangoproject.com/en/3.2/ref/models/fields/#django.db.models.Field.choices
+        ('SC', 'Soft Cover'),
+        ('HC', 'Hard Cover'),
+    ]
+    # rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    # image_url = models.URLField(max_length=1024, null=True, blank=True)
+    # image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.title
