383c3829f86b6936ce239ddcd071d8fc5de49165
ED-IP <edip_courses@outlook.es>
2021-08-13 09:45:11 +0000

    made migrations

--- products/models.py
+++ products/models.py
@@ -21,7 +21,7 @@ from django.db import models
 class Product(models.Model):
     # category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     # sku = models.CharField(max_length=254, null=True, blank=True)
-    title = models.CharField(max_length=254)
+    title = models.CharField(max_length=254, null=True)
     author = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
