41c8d1a27c2cf555877385a44cf5d6d73de3683e
ED-IP <edip_courses@outlook.es>
2021-08-14 15:45:32 +0000

    added checkout app

---
+++ checkout/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """ update order total on lineitem update/create """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def delete_on_update(sender, instance, **kwargs):  # real name should be update_on_delete
+    """ update order total on lineitem delete"""
+    instance.order.update_total()
