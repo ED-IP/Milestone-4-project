fd530e9ebefb57a951c256c5e36ccfff087e40a1
ED-IP <edip_courses@outlook.es>
2021-08-15 16:32:10 +0000

    changed model for checkout

--- checkout/models.py
+++ checkout/models.py
@@ -72,6 +72,5 @@ class OrderLineItem(models.Model):
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
-
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'SKU {self.product.title} on order {self.order.order_number}'  # changed self.product.sku for .title
--- products/admin.py
+++ products/admin.py
@@ -11,7 +11,7 @@ class ProductAdmin(admin.ModelAdmin):
         # 'category',
         'price',
         # 'rating',
-        # 'image',
+        'image',
     )
 
     # ordening = ('sku',)
