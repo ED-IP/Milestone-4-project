bbbc91eaa4fb8bf510959a17ec63202b6f833c72
ED-IP <edip_courses@outlook.es>
2021-08-16 14:44:24 +0000

    removed MEDIA_URL and MEDIA_ROOT from settings.py

--- scp_book_site/settings.py
+++ scp_book_site/settings.py
@@ -171,8 +171,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
