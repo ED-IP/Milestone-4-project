7638d216f931dd5ae662573b0d27283b9e5bddb1
ED-IP <edip_courses@outlook.es>
2021-08-16 15:33:25 +0000

    modified ALLOWED_HOSTS in settings.py

--- scp_book_site/settings.py
+++ scp_book_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://scp-book-site.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['scp-book-site.herokuapp.com', 'localhost']
 
 
 # Application definition
