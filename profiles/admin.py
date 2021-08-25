d0eb7d8cd502efbe34c4a36b72324b7bbd03408b
ED-IP <edip_courses@outlook.es>
2021-08-25 11:06:52 +0000

    added contact form to admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ContactForm
 
 # Register your models here.
+
+
+class ContactFormAdmin(admin.ModelAdmin):
+
+    fields = ('user', 'user_email', 'user_phone_number', 'description',)
+
+
+admin.site.register(ContactForm, ContactFormAdmin)
