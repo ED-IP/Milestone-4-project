36647791f65b132fd83ff3a7d61fd57df0ee6c9d
ED-IP <edip_courses@outlook.es>
2021-08-28 10:30:00 +0000

    modified STATICFILES_DIRS in settings.py

--- scp_book_site/settings.py
+++ scp_book_site/settings.py
@@ -183,7 +183,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR / 'media'),
     ]
 
 
