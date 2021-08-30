966d56ade5dd28b3d7c8948a9679284dc6b4f0bc
ED-IP <edip_courses@outlook.es>
2021-08-30 10:27:13 +0000

    fixed a typo in settings.py

--- scp_book_site/settings.py
+++ scp_book_site/settings.py
@@ -190,8 +190,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Configuration
     AWS_STORAGE_BUCKET_NAME = 'scp-book-site'
     AWS_S3_REGION_NAME = 'eu-west-3'
-    AWS_ACCESS_KEY_ID = (os.environ.get('AWS_ACCESS_KEY_ID'))
-    AWS_SECRET_ACCESS_KEY_ID = (os.environ.get('AWS_SECRET_ACCESS_KEY_ID'))
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and Media Files
